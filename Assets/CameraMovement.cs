d9b16aba1c9638e1943180eb8831402516067f01
Leo <leopold.hillion@gmail.com>
2022-01-26 22:38:46 +0100

    mouvements camera

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public GameObject Zepellin;
+    private float InitialHeight;
+
+    void Start()
+    {
+        InitialHeight = transform.position.y;
+    }
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position,Zepellin.transform.position,1*Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, InitialHeight, transform.position.z);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Zepellin.transform.rotation, 0.5f * Time.deltaTime);
+    }
+}
